0ff411d89ae9703f7dd5691e35bb3b4e890d3c35
Kamikadze9xdd <nazar.kulik99@gmail.com>
2017-12-05 18:44:32 +0200

    lab 9

--- Lab4-5/js/comment.js
+++ Lab4-5/js/comment.js
@@ -1,89 +1,119 @@
-window.addEventListener('load', function () {
-    function updateOnlineStatus(event) {
+    window.addEventListener('load', function () {
+        function updateOnlineStatus(event) {
+            if (isOnline()) {
+                readOfflineReview();
+            }
+        }
+
+        window.addEventListener('online', updateOnlineStatus);
+        window.addEventListener('offline', updateOnlineStatus);
+    });
+
+    var i = 0;
+
+    function isOnline() {
+        return window.navigator.onLine;
+    }
+
+    function addReview() {
+        if ($('#name').val() === "" || $('#text').val() === "") {
+            alert('Заповніть всі поля');
+            return false;
+        }
         if (isOnline()) {
-            readOfflineReview();
-        }
-    }
-
-    window.addEventListener('online', updateOnlineStatus);
-    window.addEventListener('offline', updateOnlineStatus);
-});
-
-var i = 0;
-
-function isOnline() {
-    return window.navigator.onLine;
-}
-
-function addComment() {
-    if ($('#username').val() === "" || $('#text').val() === "") {
-        alert('Заповніть всі поля');
-        return false;
-    }
-    if (isOnline()) {
-        var date = new Date;
-        if(date.getDate() < 10){
-          var day = '0' + date.getDate();
-        }else{
-          var day = date.getDate();
-        }
-        if(date.getMonth()<10){
-          var month = '0' + date.getMonth() + 1;
-        }else{
-          var month = date.getMonth() + 1;
-        }
-        var minute = date.getMinutes();
-        var hours = date.getHours();
-        var year = date.getFullYear();
-        var fullDate = day +'.' + month + '.' + year + "  " + hours + ":" + minute;
-        var author = document.getElementById('username').value;
-        var text = document.getElementById('text').value;
-        var parentElem = document.getElementById('comment-list');
-        var out = document.createElement('div');
-        out.id = 'comment';
-        out.innerHTML =
-          "<div><h2>" + author + " " + fullDate + "</h2><p>"
-          + text + "</p></div>";
-          parentElem.appendChild(out);
-        document.getElementById('form').reset();
-    } else {
             var date = new Date;
-            if(date.getDate() < 10){
-              var day = '0' + date.getDate();
-            }else{
-              var day = date.getDate();
-            }
-            if(date.getMonth()<10){
-              var month = '0' + date.getMonth() + 1;
-            }else{
-              var month = date.getMonth() + 1;
-            }
-            var minute = date.getMinutes();
-            var hours = date.getHours();
             var year = date.getFullYear();
-            var fullDate = day +'.' + month + '.' + year + "  " + hours + ":" + minute;
-            var author = document.getElementById('username').value;
+            if(date.getDay() < 10){
+              var day = '0' + date.getDay()
+            }else{
+              var day = date.getDay();
+            }
+            var month = date.getMonth() + 1;
+            var author = document.getElementById('name').value;
             var text = document.getElementById('text').value;
-            i++;
-            var list = [];
-            list.push({"name": author, "text": text, "date": fullDate});
-            localStorage.setItem('r' + i, JSON.stringify(list));
-            console.log("save comment in local storage")
+            var parentElem = document.getElementById('reviews-list');
+            var time = day + "." + month + "." + year;
+            var out = document.createElement('div');
+            out.id = 'review';
+            out.innerHTML =
+                "<div>" +
+                "<h2>" + author + " " + time + "</h2>"+
+                "<p>" + text + "</p></div>";
+            parentElem.appendChild(out);
+            document.getElementById('form').reset();
+        } else {
+             if (useLocalStorage) {
+                var date = new Date;
+                var year = date.getFullYear();
+                if(date.getDay() < 10){
+                  var day = '0' + date.getDay()
+                }else{
+                  var day = date.getDay();
+                }
+                var month = date.getMonth() + 1;
+                var time = day + "." + month + "." + year;
+                var author = document.getElementById('name').value;
+                var text = document.getElementById('text').value;
+                i++;
+                var list = [];
+                list.push({"name": author, "text": text, "date": time});
+                localStorage.setItem('r' + i, JSON.stringify(list));
+            } else {
+                var date = new Date;
+                var year = date.getFullYear();
+                if(date.getDay() < 10){
+                  var day = '0' + date.getDay()
+                }else{
+                  var day = date.getDay();
+                }
+                  var month = date.getMonth() + 1;
+                var time = day + "." + month + "." + year;
+                var transaction = db.transaction(["reviews"], "readwrite");
+                var store = transaction.objectStore("reviews");
+                var review = {
+                    message: document.getElementById('text').value,
+                    author: document.getElementById('name').value,
+                    time: time
+                };
+                store.add(review);
+                console.log("save in db")
+            }
+            document.getElementById('form').reset();
         }
-        document.getElementById('form').reset();
     }
 
-function readOfflineReview() {
-        len = localStorage.length + 1;
-        for (var k = 1; k < len; k++) {
-            review = JSON.parse(localStorage.getItem('r' + k));
-            var parentElem = document.getElementById('comment-list');
-            var out = document.createElement('div');
-            out.id = 'comment';
-            out.innerHTML =
-              "<div><h2>" + review[0].name + " " + review[0].date + "</h2><p>"
-              + review[0].text + "</p></div>";
-            parentElem.appendChild(out);
-            localStorage.removeItem('r' + k);
+    function readOfflineReview() {
+        if (useLocalStorage) {
+            len = localStorage.length + 1;
+            for (var k = 1; k < len; k++) {
+                review = JSON.parse(localStorage.getItem('r' + k));
+                var parentElem = document.getElementById('reviews-list');
+                var out = document.createElement('div');
+                out.id = 'review';
+                out.innerHTML =
+                "<div>" +
+                "<h2>" + review[0].name + " "+ review[0].date + "</h2>"+
+                "<p>" + review[0].text + "</p></div>";
+                parentElem.appendChild(out);
+                localStorage.removeItem(k);
+            }
+        } else {
+            var transaction = db.transaction(["reviews"], "readonly");
+            var store = transaction.objectStore("reviews");
+
+            store.openCursor().onsuccess = function (e) {
+                var cursor = e.target.result;
+                if (cursor) {
+                    cursor.continue();
+                    var parentElem = document.getElementById('reviews-list');
+                    var out = document.createElement('div');
+                    out.id = 'review';
+                    out.innerHTML =
+                    "<div>" +
+                    "<h2>" + cursor.value.author + " "+ cursor.value.time + "</h2>"+
+                    "<p>" + cursor.value.message + "</p></div>";
+                    parentElem.appendChild(out);
+                }
+            }
         }
     }
